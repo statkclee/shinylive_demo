4b31d1468912a7352aeb7d87a8741b3839fd2525
Kwangchun Lee <kwangchun.lee.7@gmail.com>
2023-10-22 23:14:20 +0900

    start shinylive

---
+++ tidyx_shinylive.R
@@ -0,0 +1,17 @@
+
+
+## the cars folder contains last weeks shiny app (bit.ly/TidyX_Ep160)
+## Convert your shiny app into all the assets for running the app 
+## in the browser
+shinylive::export(
+  appdir = "cars",
+  destdir = "docs"
+)
+
+
+## with development version of httpuv, run shinylive app locally
+## remotes::install_github("rstudio/httpuv")
+httpuv::runStaticServer(dir = "shinylive_cars", port = 8881)
+
+
+
